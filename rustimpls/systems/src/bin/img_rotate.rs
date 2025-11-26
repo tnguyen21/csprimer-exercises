f67d805879687998bbd865f05e63769a7287a1c4
Tommy Bui Nguyen <tommynguyen0512@gmail.com>
2025-11-26 11:26:13 -0600

    rotate image logic

--- rustimpls/systems/src/bin/img_rotate.rs
+++ rustimpls/systems/src/bin/img_rotate.rs
@@ -17,13 +17,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let height = u32::from_le_bytes(dib_header[4..8].try_into().unwrap());
     let width = u32::from_le_bytes(dib_header[8..12].try_into().unwrap());
 
-    let mut pixels = vec![0u8; (height * width * 3) as usize];
-    let _ = file.read_exact(&mut pixels);
+    let mut src = vec![0u8; (height * width * 3) as usize];
+    let _ = file.read_exact(&mut src);
+
+    let mut rotated = Vec::<u8>::new();
+    
+    for ty in 0..width {
+        for tx in 0..width {
+            let sy = tx;
+            let sx = width - ty - 1;
+            let n = 3 * (sy * width + sx) as usize;
+            rotated.extend_from_slice(&src[n..n+3]);
+        }
+    }
 
     let mut out_file = fs::File::create("out.bmp")?;
     out_file.write_all(&bmp_header)?;
     out_file.write_all(&dib_header)?;
-    out_file.write_all(&pixels)?;
+    out_file.write_all(&rotated)?;
 
     Ok(())
 }
