98a15c7fec571ab05a6c926a3a2d7d871a69c10e
Tommy Bui Nguyen <tommynguyen0512@gmail.com>
2025-11-26 09:53:09 -0600

    make fmt happy

--- rustimpls/systems/src/bin/beep.rs
+++ rustimpls/systems/src/bin/beep.rs
@@ -1,23 +1,23 @@
 use std::io::{self, Read, Write};
-use termios::{Termios, TCSANOW, ICANON, tcsetattr};
+use termios::{ICANON, TCSANOW, Termios, tcsetattr};
 
 fn main() -> io::Result<()> {
     let stdin = 0;
-    
+
     let mut termios = Termios::from_fd(stdin).unwrap();
-    
+
     // Disable canonical mode (line buffering)
     termios.c_lflag &= !(ICANON);
     tcsetattr(stdin, TCSANOW, &termios).unwrap();
-    
+
     let mut stdout = io::stdout();
     let mut stdin = io::stdin();
     let mut buffer = [0u8; 1];
-    
+
     loop {
         stdin.read_exact(&mut buffer)?;
         let c = buffer[0] as char;
-        
+
         if c >= '1' && c <= '9' {
             let beeps = (c as u8 - b'0') as usize;
             for _ in 0..beeps {
